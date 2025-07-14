0a76b3c7d58c0bb54a04f6403fd5881b8ebe7cf9
IDDEVELOPPEUR <samasmartsoft@gmail.com>
2025-07-14 10:43:59 +0000

    ajout du dossier environments et modeles

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Inscription} from '../modeles';
 
 @Component({
   selector: 'app-inscription',
@@ -21,6 +22,16 @@ export class InscriptionComponent {
 //la fonction inscrire
   inscrire() {
     console.log("Données formulaires :",this.inscriptionForm.value)
+    const inscription: Inscription = {
+      prenom:this.inscriptionForm.get('prenom')?.value,
+      nom:this.inscriptionForm.get('nom')?.value,
+      email:this.inscriptionForm.get('email')?.value,
+      password:this.inscriptionForm.get('password')?.value,
+      adresse:this.inscriptionForm.get('adresse')?.value,
+      roles:[],
+      droits:[]
+    }
+    console.log("send to spring",inscription)
   }
 
   //fonction pour effacer le formulaire
---
+++ src/app/modeles.ts
@@ -0,0 +1,11 @@
+
+export interface Inscription{
+
+  nom: string,
+  prenom: string,
+  email: string,
+  password: string,
+  adresse: string,
+  roles?:string[],
+  droits?: string[]
+}
